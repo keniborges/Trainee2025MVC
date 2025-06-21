c1053fa27420480e99121622b84de01f047ef71b
Keni Borges <keni@questores.com.br>
2025-06-21 08:18:45 -0300

    arquivos novos

---
+++ SenaiMvc/Models/Escola/EstadoIBGE.cs
@@ -0,0 +1,9 @@
+﻿namespace SenaiMvc.Models.Escola
+{
+    public class EstadoIBGE
+    {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+    }
+}
